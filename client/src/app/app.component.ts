566cc8b326e800babca57315a9fd38bd689cdbd0
Mikhaylov Mikhail <mihailovms@yandex.ru>
2019-02-21 02:09:46 +0300

    Add TodoService and todomvc styles to client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TodoService } from './services/todo.service';
+import { Todo } from './shared/models/todo.model';
 
 @Component({
   selector: 'td-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todomvc';
+
+  newTodo: Todo = new Todo();
+
+  constructor(private todoService: TodoService) {
+  }
+
+  addTodo() {
+    this.todoService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo: Todo) {
+    this.todoService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo: Todo) {
+    this.todoService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoService.getAllTodos();
+  }
 }
---
+++ client/src/app/services/todo.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import {Http, Response} from "@angular/http";
+import { Todo } from '../shared/models/todo.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  constructor(private http: Http) { }
+
+// Placeholder for last id so we can simulate
+  // automatic incrementing of id's
+  lastId: number = 0;
+
+  // Placeholder for todo's
+  todos: Todo[] = [];
+
+  // Simulate POST /todos
+  addTodo(todo: Todo): TodoService {
+    if (!todo.id) {
+      todo.id = ++this.lastId;
+    }
+    this.todos.push(todo);
+    return this;
+  }
+
+  // Simulate DELETE /todos/:id
+  deleteTodoById(id: number): TodoService {
+    this.todos = this.todos
+      .filter(todo => todo.id !== id);
+    return this;
+  }
+
+  // Simulate PUT /todos/:id
+  updateTodoById(id: number, values: Object = {}): Todo {
+    let todo = this.getTodoById(id);
+    if (!todo) {
+      return null;
+    }
+    Object.assign(todo, values);
+    return todo;
+  }
+
+  // Simulate GET /todos
+  getAllTodos(): Todo[] {
+    return this.todos;
+  }
+
+  // Simulate GET /todos/:id
+  getTodoById(id: number): Todo {
+    return this.todos
+      .filter(todo => todo.id === id)
+      .pop();
+  }
+
+  // Toggle todo complete
+  toggleTodoComplete(todo: Todo){
+    let updatedTodo = this.updateTodoById(todo.id, {
+      complete: !todo.complete
+    });
+    return updatedTodo;
+  }
+
+}
