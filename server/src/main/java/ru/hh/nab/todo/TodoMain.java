cadbc9882243a4ad716bfab08df47d0e6ff0eb7e
Mikhaylov Mikhail <mihailovms@yandex.ru>
2019-02-24 10:58:56 +0300

    Add serving client files

--- server/src/main/java/ru/hh/nab/todo/TodoMain.java
+++ server/src/main/java/ru/hh/nab/todo/TodoMain.java
@@ -1,13 +1,29 @@
 package ru.hh.nab.todo;
 
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.springframework.web.context.WebApplicationContext;
+
 import ru.hh.nab.starter.NabApplication;
 
 public class TodoMain {
   public static void main(String[] args) {
     NabApplication.builder()
+      .configureWebapp(TodoMain::servletContextHandlerConfigurer)
       .configureJersey(TodoJerseyConfig.class)
-      .bindToRoot()
+      .bindTo("/api/*")
       .build()
       .run(TodoConfig.class);
+  }
+
+  public static void servletContextHandlerConfigurer(WebAppContext webContext, WebApplicationContext appContext)
+  {
+    // Client static resources
+    ServletHolder clientHolder = new ServletHolder("client", DefaultServlet.class);
+    clientHolder.setInitParameter("resourceBase", "../client");
+    clientHolder.setInitParameter("dirAllowed", "true");
+    clientHolder.setInitParameter("pathInfoOnly", "true");
+    webContext.addServlet(clientHolder, "/client/*");
   }
 }
--- server/src/main/java/ru/hh/nab/todo/TodoResource.java
+++ server/src/main/java/ru/hh/nab/todo/TodoResource.java
@@ -1,22 +1,51 @@
 package ru.hh.nab.todo;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import java.util.Arrays;
+import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/")
+@Path("/todo")
 public class TodoResource {
-
+  
   @GET
-  @Path("/todo")
   @Produces(MediaType.APPLICATION_JSON)
-  public TodoDto hello() {
+  public List<TodoDto> getTodo() {
     TodoDto todo = new TodoDto();
     todo.setId(1);
     todo.setTitle("Hello World!");
     todo.setCompleted(true);
+    return Arrays.asList(todo);
+  }
+
+  @PUT
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public TodoDto addTodo(TodoDto todo) {
+    todo.setId(1);
     return todo;
   }
+
+  @POST
+  @Path("/{id}")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public void updateTodo(@PathParam("id") int id, TodoDto todo) {
+    
+  }
+
+  @DELETE
+  @Path("/{id}")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public void updateTodo(@PathParam("id") int id) {
+    
+  }
+
 }
